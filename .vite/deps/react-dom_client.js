224ec1de5c94c3a3c2a5ee8ebd2a1e68bd3f18bf
KushanEsala <124451659+KushanEsala@users.noreply.github.com>
2025-07-19 02:23:32 +0530

    commit1

---
+++ .vite/deps/react-dom_client.js
@@ -0,0 +1,7 @@
+import {
+  require_client
+} from "./chunk-D5S3K3VV.js";
+import "./chunk-5HNGYYSW.js";
+import "./chunk-TVFQMRVC.js";
+import "./chunk-G3PMV62Z.js";
+export default require_client();
